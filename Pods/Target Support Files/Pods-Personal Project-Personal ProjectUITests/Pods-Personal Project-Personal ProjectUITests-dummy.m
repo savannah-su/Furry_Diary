ffd0557b450b9f2450fa8967b6cd91077828414d
Savannah Su <tingchen0626@gmail.com>
2020-01-22 00:23:20 +0800

    import Firebase and gitignore

---
+++ Pods/Target Support Files/Pods-Personal Project-Personal ProjectUITests/Pods-Personal Project-Personal ProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Personal_Project_Personal_ProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Personal_Project_Personal_ProjectUITests
+@end
