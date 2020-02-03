fe855f15f384d4726b57e583796ae39845c9e79b
Savannah Su <tingchen0626@gmail.com>
2020-02-03 16:37:41 +0800

    finished the layout

--- Personal Project/Controller/Enter Pet Info Page/TextFieldCell.swift
+++ Personal Project/Controller/Enter Pet Info Page/TextFieldCell.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
+enum DateType {
+    
+}
+
+enum generalType{
+    
+    case optionOne
+    case optionTwo
+    case optionThree
+    
+//    var species: String {
+//        switch self {
+//        case .optionOne: return "狗狗"
+//        case .optionTwo: return "貓咪"
+//        case .optionThree: return "其他"
+//        }
+//    }
+}
+
+//var neuter: String {
+//    switch self {
+//    case .optionOne: return "尚未絕育"
+//    case .optionTwo: return "已絕育"
+//    }
+//}
+
 class TextFieldCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentField: UITextField!
     
+    var datePiker = UIDatePicker()
+    var pickerView = UIPickerView()
+    var speciesArray = ["狗狗", "貓咪", "其他"]
+    var neuterArray = ["尚未絕育", "已絕育"]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        contentField.delegate = self
+        pickerView.dataSource = self
+        pickerView.delegate = self
         // Initialization code
     }
 
@@ -24,4 +59,29 @@ class TextFieldCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension TextFieldCell: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension TextFieldCell: UIPickerViewDelegate {
+    
+}
+
+extension TextFieldCell: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return speciesArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return speciesArray[row]
+    }
 }
